89f2391bf0d396b45c151d55482410f743b2a64d
Philippe chambon <joan_krugg@live.fr>
2017-09-28 19:03:37 +0200

    add insta_link to artists

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,7 +4,8 @@ class Artist < ApplicationRecord
   validates :city, presence: true
   validates :year_of_creation, presence: true
   validates :number_of_musicians, presence: true
-  validates :facebook_link, presence: true
+  validates :facebook_link, uniqueness: true, presence: true
+  validates :insta_link, uniqueness: true
   mount_uploader :photo, PhotoUploader
   belongs_to :users, required: true
 end
