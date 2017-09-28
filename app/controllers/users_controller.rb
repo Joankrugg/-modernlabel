503ec8b0af40749835923207ba1c441f9ae065c4
Philippe chambon <joan_krugg@live.fr>
2017-09-28 14:25:42 +0200

    add description to user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,6 @@ before_action :set_user, only: [:show, :edit, :update]
   end
 
   def user_params
-    params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:description, :email)
   end
 end
