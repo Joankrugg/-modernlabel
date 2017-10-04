cb2447032c640bf5ed37f8dbf0d86f36d01fe992
Philippe chambon <joan_krugg@live.fr>
2017-10-04 12:15:10 +0200

    add first_name and last_name to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,6 +29,6 @@ before_action :set_user, only: [:show, :edit, :update]
   end
 
   def user_params
-    params.require(:user).permit(:description, :email, :city)
+    params.require(:user).permit(:description, :email, :city, :first_name, :last_name)
   end
 end
