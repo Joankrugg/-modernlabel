f1a76ef5d8ea8ee0dd44509010c079df7ccdeea1
Philippe chambon <joan_krugg@live.fr>
2017-10-22 17:27:13 +0200

    removed old db file for production

--- db/migrate/20171021163417_remove_release_ref_from_artists.rb
+++
@@ -1,6 +0,0 @@
-class RemoveReleaseRefFromArtists < ActiveRecord::Migration[5.0]
-  def change
-    remove_foreign_key :artists, column: :release_id
-    remove_column :artists, :release_id, :integer
-  end
-end
