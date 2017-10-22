0bde7360701fc0da608153504a29f91a76ff62dd
Philippe chambon <joan_krugg@live.fr>
2017-10-22 17:21:50 +0200

    removed old file for production

--- db/migrate/20171021163201_remove_performance_ref_from_artists.rb
+++
@@ -1,6 +0,0 @@
-class RemovePerformanceRefFromArtists < ActiveRecord::Migration[5.0]
-  def change
-    remove_foreign_key :artists, column: :performance_id
-    remove_column :artists, :performance_id, :integer
-  end
-end
