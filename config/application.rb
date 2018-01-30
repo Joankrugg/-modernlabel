e7828322399cce614a3b9cf2314b29af0e778733
Philippe chambon <joan_krugg@live.fr>
2018-01-30 15:28:38 +0100

    add mp3 player

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module Modernlabel
     config.action_view.embed_authenticity_token_in_remote_forms = true
 
     config.i18n.default_locale = :fr
-
+    config.assets.precompile << "audios/*"
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
