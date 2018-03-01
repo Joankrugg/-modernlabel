4eace3fd50f5abb1c5a772801b5bd7e2055ccb90
Philippe chambon <joan_krugg@live.fr>
2018-03-01 15:39:41 +0100

    try mailing on gmail server for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,14 +1,14 @@
 Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-  :port           => ENV['MAILGUN_SMTP_PORT'],
-  :address        => ENV['MAILGUN_SMTP_SERVER'],
-  :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-  :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'modernboxrecords.com',
-  :authentication => :plain,
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: 'modernboxrecords.com',
+  user_name: ENV['GMAIL_ADDRESS'],
+  password: ENV['GMAIL_APP_PASSWORD'],
+  authentication: :login,
+  enable_starttls_auto: true
 }
-ActionMailer::Base.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: "modernboxrecords.com" }
   # Settings specified here will take precedence over those in config/application.rb.
 
