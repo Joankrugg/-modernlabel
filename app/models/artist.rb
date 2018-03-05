b993709843f81893bb0cdfc214797de10c502751
Philippe chambon <joan_krugg@live.fr>
2018-03-06 00:48:52 +0100

    embed youtube frame on website

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -10,7 +10,7 @@ class Artist < ApplicationRecord
   validates :year_of_creation, presence: true
   validates :number_of_musicians, presence: true
   validates :facebook_link, uniqueness: true, presence: true, format: { with: /(?:(?:http|https):\/\/)?(?:www.)?facebook.com\/?/ }
-  validates :youtube_channel, format: { with: /(?:(?:http|https):\/\/)?(www\.youtube\.com|youtu\.?be)\// } , allow_blank: true
+  #validates :youtube_channel, format: { with: /(?:(?:http|https):\/\/)?(www\.youtube\.com|youtu\.?be)\// } , allow_blank: true
   validates :insta_link, format: { with: /(?:(?:http|https):\/\/)?(?:www.)?instagram.com\/?/ }, allow_blank: true
   validates :twitter_link, format: { with: /(?:(?:http|https):\/\/)?(?:www.)?twitter.com\/?/ }, allow_blank: true
   validates :soundcloud_link, format: { with: /(?:(?:http|https):\/\/)?(?:www.)?soundcloud.com\/?/ }, allow_blank: true
