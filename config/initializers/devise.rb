c9daf4fe1d31ae4ee90eb3828efc6f3d193670ce
Philippe chambon <joan_krugg@live.fr>
2018-03-21 22:32:08 +0100

    changes on devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -11,7 +11,8 @@ Devise.setup do |config|
     scope: 'email',
     info_fields: 'email, first_name, last_name',
     image_size: 'square',  # 50x50, guaranteed ratio
-    secure_image_url: true
+    secure_image_url: true,
+    callback_url:"https://www.domain.com/login?state="st=state313mai,ds=80879""
 
 
   # ==> Mailer Configuration
