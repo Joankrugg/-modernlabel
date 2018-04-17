81ec64ac13b6e88bcce1160199e4fa3095f77abe
Philippe chambon <joan_krugg@live.fr>
2018-04-17 16:43:48 +0200

    add validations on Video and corrections on video.new

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,4 +1,8 @@
 class Service < ApplicationRecord
   belongs_to :asso
   mount_uploader :photo, PhotoUploader
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :adhesion_fee, presence: true
+  validates :photo, presence: true
 end
