7274ef2d8c683e5ae140d5492fbad8befa6f9ad8
Philippe chambon <joan_krugg@live.fr>
2018-04-21 15:07:50 +0200

    problem on fb login

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,13 +12,7 @@ Devise.setup do |config|
     info_fields: 'email, first_name, last_name',
     image_size: 'square',  # 50x50, guaranteed ratio
     secure_image_url: true,
-    client_options: {
-      site: "https://graph.facebook.com/v2.3",
-      authorize_url: "https://www.facebook.com/v2.3/dialog/oauth"
-    },
-    token_params: {
-        parse: :json
-    }
+    callback_url: ENV["STATE"]
 
 
   # ==> Mailer Configuration
