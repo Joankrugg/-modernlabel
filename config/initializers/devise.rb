5cb7e67cd1faf95b40edf46e77c5f463b4c3efb4
Philippe chambon <joan_krugg@live.fr>
2018-04-23 18:26:09 +0200

    connection with facebook

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -11,7 +11,7 @@ Devise.setup do |config|
     scope: 'email',
     info_fields: 'email, first_name, last_name',
     image_size: 'square',  # 50x50, guaranteed ratio
-    secure_image_url: true,
+    secure_image_url: true
 
 
 
