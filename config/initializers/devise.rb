6e0dc7855ee58d6463becba152bd1bf83b5ba204
Philippe chambon <joan_krugg@live.fr>
2018-04-24 09:57:45 +0200

    changed initializers/devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -11,7 +11,14 @@ Devise.setup do |config|
     scope: 'email',
     info_fields: 'email, first_name, last_name',
     image_size: 'square',  # 50x50, guaranteed ratio
-    secure_image_url: true
+    secure_image_url: true,
+    client_options: {
+      site: "https://graph.facebook.com/v2.3",
+      authorize_url: "https://www.facebook.com/v2.3/dialog/oauth"
+    },
+    token_params: {
+      parse: :json
+    }
 
 
 
