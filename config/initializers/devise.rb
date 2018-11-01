81e089728db3af86df97609b28522602c05ba681
Philippe chambon <joan_krugg@live.fr>
2018-11-01 10:16:35 +0100

    correct typo

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -8,7 +8,7 @@
   # config.secret_key = '36753e4826ccb5df0332adc36e4352b4e6bfb0bbd987058d35002a56bf9e43c366057e2badf32fd6916b3082f0f44e0b021dcbb9dc941bc8335765bedba1e777'
 Devise.setup do |config|
   config.omniauth :facebook, ENV["FB_ID"], ENV["FB_SECRET"],
-    auth_type: 'https'
+    auth_type: 'https',
     client_options: { site: "https://graph.facebook.com/v2.1", authorize_url: "https://www.facebook.com/v2.1/dialog/oauth" },
     callback_url: 'https://modernboxrecords.com/users/auth/facebook/callback',
     scope: 'email',
