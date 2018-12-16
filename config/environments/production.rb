cf4bc4535843a7a340614ef0891982c07877aba9
Philippe chambon <joan_krugg@live.fr>
2018-12-17 00:59:51 +0100

    deploy for h cable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,8 +47,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  config.action_cable.url =  "wss://modernboxrecords.com/cable"
-  config.action_cable.allowed_request_origins = [ 'http://modernboxrecords.com', 'https://modernboxrecords.com']
+  config.action_cable.url =  "wss://modernlabel.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = [ 'http://modernlabel.herokuapp.com', 'https://modernlabel.herokuapp.com']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
