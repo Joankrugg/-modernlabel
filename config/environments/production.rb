04f27440c9c4d1dadb6e6e1a6615bb886a87114c
Philippe chambon <joan_krugg@live.fr>
2018-12-17 00:54:07 +0100

    ready to deploy on h

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,8 +47,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  config.action_cable.url =  'ws://modernboxrecords.com/cable'
-  config.action_cable.allowed_request_origins = [ 'http://modernboxrecords.com', /http:\/\/example.*/ ]
+  config.action_cable.url =  "wss://modernboxrecords.com/cable"
+  config.action_cable.allowed_request_origins = [ 'http://modernboxrecords.com', 'https://modernboxrecords.com']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
