f11dc293365f039284bc73dfa5d17b34104a082a
Philippe chambon <joan_krugg@live.fr>
2018-12-17 01:04:59 +0100

    changed wss for ws

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,7 +47,7 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  config.action_cable.url =  "wss://modernlabel.herokuapp.com/cable"
+  config.action_cable.url =  "ws://modernlabel.herokuapp.com/cable"
   config.action_cable.allowed_request_origins = [ 'http://modernlabel.herokuapp.com', 'https://modernlabel.herokuapp.com']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
