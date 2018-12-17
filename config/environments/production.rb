6accdda98fd66597e7e1f4f0481c07855b3e3322
Philippe chambon <joan_krugg@live.fr>
2018-12-17 01:58:52 +0100

    on the wy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,7 +48,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   config.action_cable.url =  "ws://modernlabel.herokuapp.com/cable"
-  config.action_cable.allowed_request_origins = [ 'http://modernlabel.herokuapp.com/pages/chat']
+  config.action_cable.allowed_request_origins = [ 'http://modernlabel.herokuapp.com']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
