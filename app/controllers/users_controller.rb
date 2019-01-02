e278922cbf77cceb0a33c8c9cddd6d47917d064e
Philippe chambon <joan_krugg@live.fr>
2019-01-02 22:55:25 +0100

    remove description from user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,6 +43,6 @@ before_action :set_user, only: [:show, :edit, :update, :destroy]
   end
 
   def user_params
-    params.require(:user).permit(:description, :email, :city, :fiveties, :sixties, :seventies, :eighties, :nineties, :millenium, :production, genre_ids:[], instrument_ids:[])
+    params.require(:user).permit(:email, :city, :fiveties, :sixties, :seventies, :eighties, :nineties, :millenium, :production, genre_ids:[], instrument_ids:[])
   end
 end
